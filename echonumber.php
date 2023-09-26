6e8ec6979c0a223a188527d44c98da967a1281bf
LarsPihl <78408273+LarsPihl@users.noreply.github.com>
2023-09-26 10:52:22 +0200

    First mostly empty files

---
+++ echonumber.php
@@ -0,0 +1,8 @@
+<?php
+require __dir__ . '/data.php';
+$number = 0;
+foreach($teams as $key) {
+    $number++;
+}
+
+echo $number;
---
+++ index.php
@@ -0,0 +1,3 @@
+require __dir__ . '/header.php';
+require __dir__ . '/data.php';
+require __dir__ . '/footer.php';
