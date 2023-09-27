7ecd5211ef639805933d17897e663adc207d2623
LarsPihl <78408273+LarsPihl@users.noreply.github.com>
2023-09-27 11:50:31 +0200

    Corrected printing error
    Corrected printing error when no value was found in array.

--- table.php
+++ table.php
@@ -23,17 +23,17 @@ require __DIR__ . '/data.php';
 
   /*The items of '$teams' are looped through using '$value => $key', in order to not only print the value found in each index's inner array, but
   also print the name of the index itself. In this case, the outer array index is the name of the team, printed as '$value', and the value of the inner
-  array is printed as '$key'.  */
+  array is printed as '$key'. If no value is found, nothing is printed inside the '<td>'-element. */
 
   foreach($teams as $value => $key) {
     ?>
     <tr>
-      <td> <?php echo $value ?></td>
-      <td> <?php echo $key['league']?></td>
-      <td> <?php echo $key['last-time-champions']?></td>
-      <td> <?php echo $key['city']?></td>
-      <td> <?php echo $key['nickname']?></td>
-      <td> <?php echo $key['url']?></td>
+      <td> <?php echo $value; ?></td>
+      <td> <?php if (!empty($key['league'])) echo $key['league'];?></td>
+      <td> <?php if (!empty($key['last-time-champions'])) echo $key['last-time-champions'];?></td>
+      <td> <?php if (!empty($key['city'])) echo $key['city'];?></td>
+      <td> <?php if (!empty($key['nickname'])) echo $key['nickname'];?></td>
+      <td> <?php if (!empty($key['url'])) echo $key['url'];?></td>
       </tr>
 
     <?php
